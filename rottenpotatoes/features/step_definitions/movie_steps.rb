5618c982e9d12c6e25033ab38a1ce573b06067cc
Richard Wallace <hunterr53@uccs.edu>
2019-03-10 18:30:22 +0000

    correct code

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -2,37 +2,48 @@
 
 Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
-    # each returned element will be a hash whose key is the table header.
-    # you should arrange to add that movie to the database here.
+    Movie.create!(:title => movie['title'],
+      :rating => movie['rating'],
+      :release_date => Time.parse(movie['release_date']))
   end
-  fail "Unimplemented"
-end
-
-Then /(.*) seed movies should exist/ do | n_seeds |
-  Movie.count.should be n_seeds.to_i
 end
 
 # Make sure that one string (regexp) occurs before or after another one
 #   on the same page
 
-Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
-  #  ensure that that e1 occurs before e2.
-  #  page.body is the entire content of the page as a string.
-  fail "Unimplemented"
+Then /^I should see "(.*)" before "(.*)" in the movie list$/ do |first,second|
+  regexp = /.*#{first}.*#{second}/
+  expect(page.text).to match(regexp)
 end
 
 # Make it easier to express checking or unchecking several boxes at once
-#  "When I uncheck the following ratings: PG, G, R"
-#  "When I check the following ratings: G"
+#  "When I check only the following ratings: PG, G, R"
 
-When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
-  # HINT: use String#split to split up the rating_list, then
-  #   iterate over the ratings and reuse the "When I check..." or
-  #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  fail "Unimplemented"
+When /I check the following ratings: (.*)/ do |rating_list|
+  # first, uncheck all the boxes
+  ['G', 'PG', 'PG-13', 'NC-17', 'R'].each do |rating|
+    uncheck("ratings[#{rating}]")
+  end
+  # now check the selected ones
+  rating_list.split(/\s*,\s*/).each do |rating|
+    check("ratings[#{rating}]")
+  end
 end
 
-Then /I should see all the movies/ do
+Then /^I should (not )?see the following movies: (.*)$/ do |no,movie_list|
+  movies = movie_list.split(/\s*,\s*/).each do |movie|
+    if no
+      expect(page).to_not have_content(movie)
+    else
+      expect(page).to have_content(movie)
+    end
+  end
+end
+
+Then /^I should see all the movies$/ do
   # Make sure that all the movies in the app are visible in the table
-  fail "Unimplemented"
+  all_titles = Movie.all.pluck(:title).join(', ')
+  steps %Q{
+  Then I should see the following movies: #{all_titles}
+}
 end
